d0b5003a2cc96ce3311fcf7ee300542dd9a433a2
OHchan <zlffjwh3@naver.com>
2021-05-21 14:13:16 +0900

    0521 git check

--- Ssaragibyul/src/main/java/com/ssaragibyul/funding/controller/FundingController.java
+++ Ssaragibyul/src/main/java/com/ssaragibyul/funding/controller/FundingController.java
@@ -33,7 +33,7 @@ import com.ssaragibyul.funding.domain.FundingReport;
 import com.ssaragibyul.funding.service.FundingService;
 import com.ssaragibyul.member.domain.Member;
 import com.ssaragibyul.visit.domain.Visit;
-
+// 깃 확인용 주석
 
 @Controller
 public class FundingController {
