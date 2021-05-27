07a2ebc92af4ea684bec2f63c1dd1e92af2724ae
OHchan <42002548+zlffjwh5@users.noreply.github.com>
2021-05-27 23:51:20 +0900

    자기전 푸시_댓글 하는듕...

--- Ssaragibyul/src/main/webapp/resources/js/fundingJoin.js
+++ Ssaragibyul/src/main/webapp/resources/js/fundingJoin.js
@@ -1,13 +1,31 @@
-$(function() {
-	$("#sendBtn").on("click", function() {
+
+$(function() {	
+	$("#sendBtn1").on("click", function() {
 		var sendNumber = $("#sendNumber").val();
 		var price = $("#price").val();
 		
-		var result = sendNumber*price;
+		var result1 = sendNumber*price;
 		
 		$("#test").text(sendNumber);
 		
 		$(".getNumber").val(sendNumber);
-		$(".getResult").val(result);
+		$(".getResult1").val(result1);
+	});
+});
+
+$(function() {
+	$("#sendBtn2").on("click", function() {
+		var sendNumber = $("#sendNumber").val();
+		var price = $("#price").val();
+		var addPrice = $("#addPrice").val();
+		
+		var result = sendNumber * price ;
+		
+		var reuslt1 = Number(addPrice);
+		
+		var result2 = result + reuslt1 ;
+		
+		$(".getNumber").val(sendNumber);
+		$(".getResult2").val(result2);
 	});
 });
